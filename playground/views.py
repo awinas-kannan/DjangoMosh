4e91e1d1d4157997346f0f852f635237522c3425
Awinas kannan Manicka rajan - a0m0rtj <Awinas.Kannan.Manicka.Rajan@walmart.com>
2023-11-26 23:08:46 +0530

    update

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def say_hello(request):
+  # Pull data from data base
+  # Sent mail
+  # Trnasform data
+  return HttpResponse("Hello Mosh")
