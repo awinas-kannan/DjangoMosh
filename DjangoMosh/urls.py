46433fa0909e7d836db11a390425fa705e29a232
Awinas kannan Manicka rajan - a0m0rtj <Awinas.Kannan.Manicka.Rajan@walmart.com>
2023-11-26 23:32:08 +0530

    Debug

--- DjangoMosh/urls.py
+++ DjangoMosh/urls.py
@@ -20,5 +20,6 @@ from django.urls import path ,include
 
 urlpatterns = [
      path('playground/', include('playground.urls')),
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
+     path("__debug__/", include("debug_toolbar.urls")),
 ]
--- playground/views.py
+++ playground/views.py
@@ -6,4 +6,9 @@ def say_hello(request):
   # Pull data from data base
   # Sent mail
   # Trnasform data
+  x =1
+  y =10
   return HttpResponse("Hello Mosh")
+
+def hellohtml(request):
+  return render(request,"hello.html")
